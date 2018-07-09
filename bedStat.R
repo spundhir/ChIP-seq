6314dc238946f7265b4fc40ebe97d35d52824615
Sachin Pundhir <pundhir@lobster.bric.dk>
2018-07-09 17:24:28 +0200

    Update Mon Jul  9 17:24:28 CEST 2018

--- bedStat.R
+++ bedStat.R
@@ -34,6 +34,8 @@ if(!is.null(opt$list)){
     cat(sprintf("Max=%0.0f\n", max(data$V1)))
     cat(sprintf("Min=%0.0f\n", min(data$V1)))
     cat(sprintf("Count=%0.0f\n", length(data$V1)))
+    cat(sprintf("01 quantile=%0.0f\n", quantile(data$V1, probs=0.01, type=8)[[1]]))
+    cat(sprintf("05 quantile=%0.0f\n", quantile(data$V1, probs=0.05, type=8)[[1]]))
     cat(sprintf("95 quantile=%0.0f\n", quantile(data$V1, probs=0.95, type=8)[[1]]))
     cat(sprintf("99 quantile=%0.0f\n", quantile(data$V1, probs=0.99, type=8)[[1]]))
 }else{
@@ -42,6 +44,8 @@ if(!is.null(opt$list)){
     cat(sprintf("Max=%0.0f\n", max(data$V3-data$V2)))
     cat(sprintf("Min=%0.0f\n", min(data$V3-data$V2)))
     cat(sprintf("Count=%0.0f\n", length(data$V3-data$V2)))
+    cat(sprintf("01 quantile=%0.0f\n", quantile(data$V3-data$V2, probs=0.01, type=8)[[1]]))
+    cat(sprintf("05 quantile=%0.0f\n", quantile(data$V3-data$V2, probs=0.05, type=8)[[1]]))
     cat(sprintf("95 quantile=%0.0f\n", quantile(data$V3-data$V2, probs=0.95, type=8)[[1]]))
     cat(sprintf("99 quantile=%0.0f\n", quantile(data$V3-data$V2, probs=0.99, type=8)[[1]]))
 }
