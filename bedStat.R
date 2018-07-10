2788c8fd724c09a66ff4cd0d9f924635ad54a427
Sachin Pundhir <pundhir@lobster.bric.dk>
2018-07-10 10:06:58 +0200

    Update Tue Jul 10 10:06:58 CEST 2018

--- bedStat.R
+++ bedStat.R
@@ -28,16 +28,16 @@ if(identical(opt$bedFile, "stdin")==T) {
 }
 
 if(!is.null(opt$list)){
-    cat(sprintf("Median=%0.0f\n", median(data$V1)))
-    cat(sprintf("Mean=%0.0f\n", mean(data$V1)))
-    cat(sprintf("Sum=%0.0f\n", sum(data$V1)))
-    cat(sprintf("Max=%0.0f\n", max(data$V1)))
-    cat(sprintf("Min=%0.0f\n", min(data$V1)))
-    cat(sprintf("Count=%0.0f\n", length(data$V1)))
-    cat(sprintf("01 quantile=%0.0f\n", quantile(data$V1, probs=0.01, type=8)[[1]]))
-    cat(sprintf("05 quantile=%0.0f\n", quantile(data$V1, probs=0.05, type=8)[[1]]))
-    cat(sprintf("95 quantile=%0.0f\n", quantile(data$V1, probs=0.95, type=8)[[1]]))
-    cat(sprintf("99 quantile=%0.0f\n", quantile(data$V1, probs=0.99, type=8)[[1]]))
+    cat(sprintf("Median=%0.4f\n", median(data$V1)))
+    cat(sprintf("Mean=%0.4f\n", mean(data$V1)))
+    cat(sprintf("Sum=%0.4f\n", sum(data$V1)))
+    cat(sprintf("Max=%0.4f\n", max(data$V1)))
+    cat(sprintf("Min=%0.4f\n", min(data$V1)))
+    cat(sprintf("Count=%0.4f\n", length(data$V1)))
+    cat(sprintf("01 quantile=%0.4f\n", quantile(data$V1, probs=0.01, type=8)[[1]]))
+    cat(sprintf("05 quantile=%0.4f\n", quantile(data$V1, probs=0.05, type=8)[[1]]))
+    cat(sprintf("95 quantile=%0.4f\n", quantile(data$V1, probs=0.95, type=8)[[1]]))
+    cat(sprintf("99 quantile=%0.4f\n", quantile(data$V1, probs=0.99, type=8)[[1]]))
 }else{
     cat(sprintf("Median=%0.0f\n", median(data$V3-data$V2)))
     cat(sprintf("Mean=%0.0f\n", mean(data$V3-data$V2)))
